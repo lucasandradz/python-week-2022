6fa5ed9075325a68e3445cb258a96171da649d67
Lucas Andrade <lucasandradz@gmail.com>
2022-05-11 17:42:25 -0300

    feat: Removing sqlmodels inherit_cache warnings.

--- beerlog/database.py
+++ beerlog/database.py
@@ -1,8 +1,19 @@
+import warnings
+
+from sqlalchemy import true
+from sqlalchemy.exc import SAWarning
 from sqlmodel import Session, create_engine
+from sqlmodel.sql.expression import Select, SelectOfScalar
 
 from beerlog import models
 from beerlog.config import settings
 
+# Removing warnings.
+warnings.filterwarnings("ignore", category=SAWarning)
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
+
+# Creating database engine
 engine = create_engine(settings.database.url)
 
 models.SQLModel.metadata.create_all(engine)
